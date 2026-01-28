4839f9687b8a4084cd38667bca0472ea981278b7
Ketaki Kamble <ketaki838@gmail.com>
2026-01-28 09:18:39 +0530

    Time: 750 ms (7.43%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT DISTINCT customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT customer.product_key) = (SELECT COUNT(*) FROM Product)
+;
