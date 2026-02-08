3801e8e98757a5cb5c8b1dfdb5c7ae1c46a2d688
Ketaki Kamble <ketaki838@gmail.com>
2026-02-08 21:24:37 +0530

    Time: 1539 ms (5.05%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+select id, sum(num) as num
+from (
+    (select requester_id as id, count(*) as num
+     from RequestAccepted
+     group by requester_id)
+    union all
+    (select accepter_id as id, count(*) as num
+     from RequestAccepted
+     group by accepter_id)
+) t
+group by id
+order by sum(num) desc
+limit 1;
